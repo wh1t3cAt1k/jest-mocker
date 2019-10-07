b9673c1a7b47f172748dff077600a54832bf5b03
wh1t3cAt1k <pavel@kabir.ru>
2019-10-07 22:30:34 +0300

    Add more methods

--- README.md
+++ README.md
@@ -2,13 +2,22 @@
 
 A mocking helper for those who would like to explicitly mock modules preserving compile-time type and refactoring safety.
 
-# Usage
+## Usage
 
 Inject a `Mocker` instance as a global (e.g. `mocker`) to use during your tests.
 
-Then you can use e.g. `mocker.mockAllFunctionsInObject(ClassOrObject)` to replace all functions
-within that object with mocks.
+Then you can use e.g. `mocker.mockAllFunctionsInObject(ClassOrObject)` to replace all functions within that object with mocks.
 
 The function returns a strongly typed object containing mocks, so you can setup and verify calls on them.
 
 On the other hand, you can continue invoking any method as usual through `ClassOrObject`, and your mocks will be called instead.
+
+## API
+
+1. `mockAllFunctionsInObject` - mocks all functions in an object and resets their default implementation to always returning `undefined`.
+1. `spyOnAllFunctionsInObject` - mocks all functions in an object and keeps their real implementation by default.
+1. `mockOneFunctionInObject` - mocks just one function in an object and resets its default implementation to always returning `undefined`. The rest of the functions are not mocked.
+1. `mockAllButOneFunctionInObject` - mocks all functions but one in an object and resets their default implementation to always returning `undefined`. The remaining function is not mocked.
+1. `spyOnAllButOneFunctionInObject` - mocks all functions except one in an object and keeps their real implementation by default. The remaining function is not mocked.
+
+All methods return a strongly typed object with relevant functions marked as mock instances.
--- src/Mocker.ts
+++ src/Mocker.ts
@@ -1,11 +1,13 @@
 type ArgsType<T> = T extends (...args: infer A) => any ? A : never;
 
-export type FunctionMock<TFunction extends (...args: any) => any> = jest.Mock<ReturnType<TFunction>, ArgsType<TFunction>>;
+export type FunctionMock<
+    TFunction extends (...args: any) => any
+> = jest.MockInstance<ReturnType<TFunction>, ArgsType<TFunction>>;
 
 export type FunctionsReplacedWithMocks<TObject> = {
     [TKey in keyof TObject]: TObject[TKey] extends (...args: any) => any
         ? FunctionMock<TObject[TKey]>
-        : TObject[TKey]
+        : TObject[TKey];
 };
 
 type FunctionMockWhenKeyMatches<
@@ -34,8 +36,9 @@ export type OneFunctionReplacedWithMock<
 > = {
     [TKey in keyof TObject]: TObject[TKey] extends (...args: any) => any
         ? FunctionMockWhenKeyMatches<TObject[TKey], TKey, TFunctionKey>
-        : TObject[TKey]
-};
+        : TObject[TKey];
+} &
+    TObject;
 
 export type AllButOneFunctionReplacedWithMocks<
     TObject,
@@ -43,19 +46,64 @@ export type AllButOneFunctionReplacedWithMocks<
 > = {
     [TKey in keyof TObject]: TObject[TKey] extends (...args: any) => any
         ? FunctionMockWhenKeyDoesNotMatch<TObject[TKey], TKey, TFunctionKey>
-        : TObject[TKey]
-};
+        : TObject[TKey];
+} &
+    TObject;
 
 /**
- * The mocker helper class. Make it an available global in your tests 
+ * The mocker helper class. Make it an available global in your tests
  * to leverage its enormous and beautiful functionality.
  */
 export class Mocker {
+    /**
+     * Mutably mocks all functions in an object by spying on them and setting them
+     * up to always return `undefined` as the default implementation.
+     *
+     * @returns The same instance of object, modified, typed as an object containing mocks.
+     */
     public mockAllFunctionsInObject = <TObject>(
         object: TObject
     ): jest.Mocked<typeof object> =>
-        this.conditionallyMockFunctionsInObject(object, _key => true);
+        this.conditionallyMockFunctionsInObject(object, _key => true, true);
 
+    /**
+     * Mutably mocks all functions in an object by spying on them and keeping
+     * their real implementation by default.
+     *
+     * @returns The same instance of object, modified, typed as an object containing mocks.
+     */
+    public spyOnAllFunctionsInObject = <TObject>(
+        object: TObject
+    ): jest.Mocked<typeof object> =>
+        this.conditionallyMockFunctionsInObject(object, _key => true, false);
+
+    /**
+     * Mutably mocks all functions but one in an object by spying on them and
+     * keeping their real implementation by default.
+     *
+     * @returns The same instance of object, modified, typed as an object
+     * containing relevant mocks.
+     */
+    public spyOnAllButOneFunctionInObject = <
+        TObject,
+        TFunctionKey extends keyof TObject
+    >(
+        object: TObject,
+        functionName: TFunctionKey
+    ): AllButOneFunctionReplacedWithMocks<typeof object, TFunctionKey> =>
+        this.conditionallyMockFunctionsInObject(
+            object,
+            key => key === functionName,
+            false
+        );
+
+    /**
+     * Mocks just one function in an object by spying on it and setting it up to always
+     * return `undefined` as the default implementation.
+     *
+     * @returns The same instance of object, modified, with the mocked function
+     * strongly typed as a mock.
+     */
     public mockOneFunctionInObject = <
         TObject extends {},
         TFunctionKey extends keyof TObject
@@ -65,9 +113,17 @@ export class Mocker {
     ): OneFunctionReplacedWithMock<typeof object, TFunctionKey> =>
         this.conditionallyMockFunctionsInObject(
             object,
-            key => key === functionName
+            key => key === functionName,
+            true
         );
 
+    /**
+     * Mocks all functions except one in an object by spying on them and
+     * setting them up to always return `undefined` as the default implementation.
+     *
+     * @returns The same instance of object, modified, with relevant mocked
+     * functions strongly typed as mocks.
+     */
     public mockAllButOneFunctionInObject = <
         TObject extends {},
         TFunctionKey extends keyof TObject
@@ -77,7 +133,8 @@ export class Mocker {
     ): AllButOneFunctionReplacedWithMocks<typeof object, TFunctionKey> =>
         this.conditionallyMockFunctionsInObject(
             object,
-            key => key !== functionName
+            key => key !== functionName,
+            true
         );
 
     /**
@@ -86,9 +143,10 @@ export class Mocker {
      */
     private readonly conditionallyMockFunctionsInObject = <TObject extends {}>(
         object: TObject,
-        predicate: (keyName: string) => boolean
+        predicate: (keyName: string) => boolean,
+        shouldResetRealImplementation: boolean
     ): any => {
-        const objectAsAny = <any>object;
+        const objectAsAny = object as any;
 
         Object.getOwnPropertyNames(object)
             .filter(key => typeof objectAsAny[key] === 'function')
@@ -97,10 +155,14 @@ export class Mocker {
                     (objectAsAny[key] as jest.Mock).mockRestore();
                 }
 
-                if (predicate(key)) {
-                    jest.spyOn(objectAsAny, key).mockImplementation(
-                        () => undefined
-                    );
+                if (!predicate(key)) {
+                    return;
+                }
+
+                const spy = jest.spyOn(objectAsAny, key);
+
+                if (shouldResetRealImplementation) {
+                    spy.mockImplementation(() => undefined);
                 }
             });
 
